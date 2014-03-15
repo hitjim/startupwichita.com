6f179df41f6c5164ab590c7dad5680b72691fabf
Jim Rice <hitjim@gmail.com>
2014-03-15 13:31:12 -0500

    Issue #26 Added News service ... and some derp

---
+++ public/js/services/news.js
@@ -0,0 +1,24 @@
+(function(angular) {
+    'use strict';
+
+    // News service used for articles REST endpoint
+    var NewsService = [
+        '$resource',
+        function($resource) {
+            return $resource('/api/v1/news/:newsId', {
+                id: '@_id'
+            }, {
+                update: {
+                    method: 'PUT'
+                },
+                search: {
+                    method: 'GET',
+                    isArray: true
+                
+                }
+            });
+        }
+    ];
+
+    angular.module('startupwichita.services').factory('News', NewsService);
+})(window.angular);
---
+++ test/karma/unit/controllers/news.spec.js
@@ -0,0 +1,198 @@
+'use strict';
+
+(function() {
+    // News Controller Spec
+    describe('startupwichita controllers', function() {
+        describe('NewsController', function() {
+            // The $resource service augments the response object with methods for updating and deleting the resource.
+            // If we were to use the standard toEqual matcher, our tests would fail because the test values would not match
+            // the responses exactly. To solve the problem, we use a newly-defined toEqualData Jasmine matcher.
+            // When the toEqualData matcher compares two objects, it takes only object properties into
+            // account and ignores methods.
+            beforeEach(function() {
+                this.addMatchers({
+                    toEqualData: function(expected) {
+                        return angular.equals(this.actual, expected);
+                    }
+                });
+            });
+
+            // Load the controllers module
+            beforeEach(module('startupwichita'));
+
+            // Initialize the controller and a mock scope
+            var NewsController,
+                scope,
+                $httpBackend,
+                $stateParams,
+                $location;
+
+            // The injector ignores leading and trailing underscores here (i.e. _$httpBackend_).
+            // This allows us to inject a service but then attach it to a variable
+            // with the same name as the service.
+            beforeEach(inject(function($controller, $rootScope, _$location_, _$stateParams_, _$httpBackend_) {
+
+                scope = $rootScope.$new();
+
+                NewsController = $controller('NewsController', {
+                    $scope: scope
+                });
+
+                $stateParams = _$stateParams_;
+
+                $httpBackend = _$httpBackend_;
+
+                $location = _$location_;
+
+            }));
+
+            it('$scope.find() should create an array with at least one newsItem object ' +
+                'fetched from XHR', function() {
+
+                    // test expected GET request
+                    $httpBackend.expectGET('news').respond([{
+                        title: 'An NewsItem about MEAN',
+                        content: 'MEAN rocks!'
+                    }]);
+
+                    // run controller
+                    scope.find();
+                    $httpBackend.flush();
+
+                    // test scope value
+                    expect(scope.news).toEqualData([{
+                        title: 'An NewsItem about MEAN',
+                        content: 'MEAN rocks!'
+                    }]);
+
+                });
+
+            it('$scope.findOne() should create an array with one newsItem object fetched ' +
+                'from XHR using a newsId URL parameter', function() {
+                    // fixture URL parament
+                    $stateParams.newsId = '525a8422f6d0f87f0e407a33';
+
+                    // fixture response object
+                    var testNewsData = function() {
+                        return {
+                            title: 'An NewsItem about MEAN',
+                            content: 'MEAN rocks!'
+                        };
+                    };
+
+                    // test expected GET request with response object
+                    $httpBackend.expectGET(/news\/([0-9a-fA-F]{24})$/).respond(testNewsData());
+
+                    // run controller
+                    scope.findOne();
+                    $httpBackend.flush();
+
+                    // test scope value
+                    expect(scope.newsItem).toEqualData(testNewsData());
+
+                });
+
+            it('$scope.create() with valid form data should send a POST request ' +
+                'with the form input values and then ' +
+                'locate to new object URL', function() {
+
+                    // fixture expected POST data
+                    var postNewsData = function() {
+                        return {
+                            title: 'An NewsItem about MEAN',
+                            content: 'MEAN rocks!'
+                        };
+                    };
+
+                    // fixture expected response data
+                    var responseNewsData = function() {
+                        return {
+                            _id: '525cf20451979dea2c000001',
+                            title: 'An NewsItem about MEAN',
+                            content: 'MEAN rocks!'
+                        };
+                    };
+
+                    // fixture mock form input values
+                    scope.title = 'An NewsItem about MEAN';
+                    scope.content = 'MEAN rocks!';
+
+                    // test post request is sent
+                    $httpBackend.expectPOST('news', postNewsData()).respond(responseNewsData());
+
+                    // Run controller
+                    scope.create();
+                    $httpBackend.flush();
+
+                    // test form input(s) are reset
+                    expect(scope.title).toEqual('');
+                    expect(scope.content).toEqual('');
+
+                    // test URL location to new object
+                    expect($location.path()).toBe('/news/' + responseNewsData()._id);
+                });
+
+            it('$scope.update() should update a valid newsItem', inject(function(News) {
+
+                // fixture rideshare
+                var putNewsData = function() {
+                    return {
+                        _id: '525a8422f6d0f87f0e407a33',
+                        title: 'An NewsItem about MEAN',
+                        to: 'MEAN is great!'
+                    };
+                };
+
+                // mock newsItem object from form
+                var newsItem = new News(putNewsData());
+
+                // mock newsItem in scope
+                scope.newsItem = newsItem;
+
+                // test PUT happens correctly
+                $httpBackend.expectPUT(/news\/([0-9a-fA-F]{24})$/).respond();
+
+                // testing the body data is out for now until an idea for testing the dynamic updated array value is figured out
+                //$httpBackend.expectPUT(/news\/([0-9a-fA-F]{24})$/, putNewsData()).respond();
+                /*
+                Error: Expected PUT /news\/([0-9a-fA-F]{24})$/ with different data
+                EXPECTED: {"_id":"525a8422f6d0f87f0e407a33","title":"An NewsItem about MEAN","to":"MEAN is great!"}
+                GOT:      {"_id":"525a8422f6d0f87f0e407a33","title":"An NewsItem about MEAN","to":"MEAN is great!","updated":[1383534772975]}
+                */
+
+                // run controller
+                scope.update();
+                $httpBackend.flush();
+
+                // test URL location to new object
+                expect($location.path()).toBe('/news/' + putNewsData()._id);
+
+            }));
+
+            it('$scope.remove() should send a DELETE request with a valid newsId' +
+                'and remove the newsItem from the scope', inject(function(News) {
+
+                    // fixture rideshare
+                    var newsItem = new News({
+                        _id: '525a8422f6d0f87f0e407a33'
+                    });
+
+                    // mock rideshares in scope
+                    scope.news = [];
+                    scope.news.push(newsItem);
+
+                    // test expected rideshare DELETE request
+                    $httpBackend.expectDELETE(/news\/([0-9a-fA-F]{24})$/).respond(204);
+
+                    // run controller
+                    scope.remove(newsItem);
+                    $httpBackend.flush();
+
+                    // test after successful delete URL location news lis
+                    //expect($location.path()).toBe('/news');
+                    expect(scope.news.length).toBe(0);
+
+                }));
+        });
+    });
+}());
